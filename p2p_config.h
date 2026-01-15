24b79880b0c8afa9bd0049c8b3b2aef49411a464
GoGoogle <wowjoint@gmail.com>
2026-01-15 09:35:35 +0800

    Update p2p_config.h

--- p2p_config.h
+++ p2p_config.h
@@ -4,23 +4,25 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <shellapi.h>
+#include <commctrl.h> // 新增: 界面控件支持
 #include <stdio.h>
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "user32.lib")
-#pragma comment(lib, "shell32.lib")
 #pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "comctl32.lib") // 新增: 界面库
 
-// --- 调试配置 ---
-#define ENABLE_TRAY   1       // 1: 显示托盘图标, 0: 完全隐藏
+// --- 调试开关 ---
+#define ENABLE_TRAY   1       // 1: 开启托盘右键退出 (调试用)
 
-// --- 资源 ID (务必与 rc 文件严格对应) ---
+// --- 资源 ID ---
 #define IDD_LOGIN     101
 #define IDC_EDIT_ID   1001
 #define ID_TRAY_EXIT  2001
 
-// --- 按照您的要求配置的 9 个 STUN 地址 ---
+// --- 完整的 STUN 列表 (按要求配置) ---
 static const char* STUN_SERVERS[] = {
     "stun.qq.com:3478",
     "stun.aliyun.com:3478",
@@ -38,12 +40,11 @@ static const char* STUN_SERVERS[] = {
 #define LAN_PORT      8888
 #define AUTH_MAGIC    0xABCDEF12
 
+// --- 协议定义 ---
 #pragma pack(push, 1)
 typedef struct {
-    unsigned short type;
-    unsigned short length; 
-    unsigned int magic;
-    unsigned char id[12];
+    unsigned short type; unsigned short length; 
+    unsigned int magic; unsigned char id[12];
 } StunHeader;
 #pragma pack(pop)
 
@@ -53,8 +54,8 @@ typedef struct {
     int x, y;
 } P2PPacket;
 
-#define CLR_LAN       RGB(0, 255, 255)
-#define CLR_WAN       RGB(0, 255, 0)
-#define CLR_TRY       RGB(255, 255, 0)
+#define CLR_LAN       RGB(0, 255, 255) // 青色
+#define CLR_WAN       RGB(0, 255, 0)   // 绿色
+#define CLR_TRY       RGB(255, 255, 0) // 黄色
 
 #endif
