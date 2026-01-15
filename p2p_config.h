16179834540ec8e3a3e9ebb2a5363432b8b22d21
GoGoogle <wowjoint@gmail.com>
2026-01-15 11:21:28 +0800

    Update p2p_config.h

--- p2p_config.h
+++ p2p_config.h
@@ -18,12 +18,16 @@
 #pragma comment(lib, "gdiplus.lib")
 #pragma comment(lib, "ole32.lib")
 
+// --- 全局配置 ---
 #define ENABLE_TRAY   1
+
+// --- 资源 ID ---
 #define IDD_LOGIN     101
 #define IDC_EDIT_ID   1001
 #define ID_TRAY_EXIT  2001
 #define WM_TRAY_MSG   (WM_USER + 2)
 
+// --- STUN 配置 (2026-01-15) ---
 static const char* STUN_SERVERS[] = {
     "stun.qq.com:3478", "stun.aliyun.com:3478", "stun.huawei.com:3478",
     "stun.cloudflare.com:3478", "stun.l.google.com:19302", "stun1.l.google.com:19302",
@@ -38,11 +42,11 @@ static const char* STUN_SERVERS[] = {
 #pragma pack(push, 1)
 typedef struct {
     unsigned int magic;
-    int type;       
-    int frame_id;   
-    int offset;     
-    int total_size; 
-    int slice_size; 
+    int type;       // 1: CMD, 2: SCREEN_JPEG
+    int frame_id;   // 帧序号
+    int offset;     // 字节偏移
+    int total_size; // JPEG 总大小
+    int slice_size; // 本包大小
     unsigned char data[CHUNK_SIZE];
 } P2PPacket;
 #pragma pack(pop)
