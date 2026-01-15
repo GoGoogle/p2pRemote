e1ab8a146af841bb67df692dcc3ba39adb5add5b
GoGoogle <wowjoint@gmail.com>
2026-01-15 09:22:31 +0800

    Update p2p_config.h

--- p2p_config.h
+++ p2p_config.h
@@ -3,16 +3,24 @@
 
 #include <winsock2.h>
 #include <windows.h>
+#include <shellapi.h>
 #include <stdio.h>
+#include <ws2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "shell32.lib")
+
+// --- 调试配置开关 ---
+#define ENABLE_TRAY   1       // 设置为 1 开启托盘退出选项，设置为 0 则完全静默后台
 
 // --- 资源 ID 定义 ---
 #define IDD_LOGIN     101
 #define IDC_EDIT_ID   1001
 #define IDC_STATUS    1002
+#define ID_TRAY_EXIT  2001    // 托盘退出菜单 ID
 
-// --- 绝对完整的 STUN 服务器列表 (含 Google 全家桶) ---
+// --- 完整的 STUN 服务器列表 (按照您的要求排序) ---
 static const char* STUN_SERVERS[] = {
     "stun.qq.com:3478",
     "stun.aliyun.com:3478",
@@ -26,35 +34,27 @@ static const char* STUN_SERVERS[] = {
 };
 #define STUN_COUNT 9
 
-// --- 网络端口与魔数 ---
 #define P2P_PORT      9000
 #define LAN_PORT      8888
 #define AUTH_MAGIC    0xABCDEF12
 
-// --- STUN 报文头定义 (RFC 5389) ---
 #pragma pack(push, 1)
 typedef struct {
-    unsigned short type;   // 0x0001: Binding Request
+    unsigned short type;
     unsigned short length; 
-    unsigned int magic;    // 0x2112A442
+    unsigned int magic;
     unsigned char id[12];
 } StunHeader;
 #pragma pack(pop)
 
-// --- 核心协议封包结构 ---
 typedef struct {
     unsigned int magic;
-    int type; // 1:移动, 2:左键点击
-    int x;
-    int y;
-    int data_len;
-    char data[1024];
+    int type; // 1:移动, 2:点击
+    int x, y;
 } P2PPacket;
 
-// --- 指示颜色 ---
-#define CLR_LAN       RGB(0, 255, 255) // 青色：局域网
-#define CLR_WAN       RGB(0, 255, 0)   // 绿色：公网
-#define CLR_TRY       RGB(255, 255, 0) // 黄色：握手
-#define CLR_ERR       RGB(255, 0, 0)   // 红色：错误
+#define CLR_LAN       RGB(0, 255, 255)
+#define CLR_WAN       RGB(0, 255, 0)
+#define CLR_TRY       RGB(255, 255, 0)
 
 #endif
