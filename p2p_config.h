f8c63aef96ebb2cf9df1ba78e5b6b024ada96fee
GoGoogle <wowjoint@gmail.com>
2026-01-15 08:52:08 +0800

    Update p2p_config.h

--- p2p_config.h
+++ p2p_config.h
@@ -3,38 +3,44 @@
 
 #include <winsock2.h>
 #include <windows.h>
-#include <stdio.h>
 
-#pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "user32.lib")
-#pragma comment(lib, "gdi32.lib")
-#pragma comment(lib, "advapi32.lib")
-#pragma comment(lib, "ole32.lib")
+// --- 资源 ID 定义 (供 resource.rc 和 client.c 使用) ---
+#define IDD_LOGIN     101
+#define IDC_EDIT_ID   1001
 
+// --- 网络配置 ---
 static const char* STUN_SERVERS[] = {
-     "stun.aliyun.com:3478", "stun.qq.com:3478", "stun.huawei.com:3478", "stun.l.google.com:19302"
+    "stun.qq.com:3478", 
+    "stun.aliyun.com:3478", 
+    "stun.huawei.com:3478",
+    "stun.cloudflare.com:3478", 
+    "stun.l.google.com:19302"
 };
-#define STUN_COUNT 4
-#define P2P_PORT 9000
-#define LAN_PORT 8888
+#define STUN_COUNT 5
+#define P2P_PORT   9000
+#define LAN_PORT   8888
 #define AUTH_MAGIC 0xABCDEF12
 
-// 状态色
-#define CLR_LAN  RGB(0, 255, 255)
-#define CLR_WAN  RGB(0, 255, 0)
-#define CLR_TRY  RGB(255, 255, 0)
-#define CLR_ERR  RGB(255, 0, 0)
+// --- 状态颜色 ---
+#define CLR_LAN       RGB(0, 255, 255) // 青色：局域网
+#define CLR_WAN       RGB(0, 255, 0)   // 绿色：公网直连
+#define CLR_TRY       RGB(255, 255, 0) // 黄色：尝试中
+#define CLR_ERR       RGB(255, 0, 0)   // 红色：离线
 
+// --- 数据协议结构 ---
 typedef struct {
     unsigned int magic;
-    int type; // 0:心跳, 1:移动, 2:点击, 3:剪贴板
-    int x, y;
-    int data_len;
-    char data[1024];
+    int type;      // 0:心跳, 1:移动, 2:点击, 3:剪贴板同步
+    int x;
+    int y;
+    int data_len;  // 针对剪贴板文本长度
+    char data[1024]; // 密信传输缓冲区
 } P2PPacket;
 
+// --- 安全加密逻辑 ---
 static void SecureXOR(char* d, int l) {
-    const char k[] = "Gemini_P2P_2026";
+    const char k[] = "Gemini_P2P_2026"; // 你的专属通信密匙
+    if (l <= 0) return;
     for(int i=0; i<l; i++) d[i] ^= k[i % 15];
 }
 
