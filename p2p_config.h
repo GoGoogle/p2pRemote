cad09a08b3eefa95c7220e6faf0d2299bdec260c
GoGoogle <wowjoint@gmail.com>
2026-01-15 10:19:19 +0800

    Update p2p_config.h

--- p2p_config.h
+++ p2p_config.h
@@ -13,12 +13,12 @@
 #pragma comment(lib, "shell32.lib")
 #pragma comment(lib, "comctl32.lib")
 
+#define ENABLE_TRAY   1
 #define IDD_LOGIN     101
 #define IDC_EDIT_ID   1001
 #define ID_TRAY_EXIT  2001
 #define WM_TRAY_MSG   (WM_USER + 2)
 
-// STUN 列表 (已按要求更新)
 static const char* STUN_SERVERS[] = {
     "stun.qq.com:3478", "stun.aliyun.com:3478", "stun.huawei.com:3478",
     "stun.cloudflare.com:3478", "stun.l.google.com:19302", "stun1.l.google.com:19302",
@@ -27,26 +27,17 @@ static const char* STUN_SERVERS[] = {
 
 #define P2P_PORT      9000
 #define AUTH_MAGIC    0xABCDEF12
-
-// 数据包类型
-#define PKT_CMD       1
-#define PKT_SCREEN    2
+#define CHUNK_SIZE    1024  // 每个 UDP 包的大小
 
 #pragma pack(push, 1)
 typedef struct {
     unsigned int magic;
-    int type; // PKT_CMD
-    int cmd;  // 1:移动+左击
-    int x, y;
-} CommandPacket;
-
-typedef struct {
-    unsigned int magic;
-    int type; // PKT_SCREEN
-    int offset;
-    int size;
-    unsigned char data[1024]; // 简单分片发送
-} ScreenPacket;
+    int type; // 1: CMD, 2: SCREEN
+    int x, y; // 对于 CMD 是坐标，对于 SCREEN 是分片偏移和总大小
+    int slice_size;
+    unsigned char data[CHUNK_SIZE];
+} P2PPacket;
 #pragma pack(pop)
 
+#define CLR_ACTIVE    RGB(0, 255, 0)
 #endif
