342844e1dcc763b573c555fab4bb04e7b3b6fe63
GoGoogle <wowjoint@gmail.com>
2026-01-15 11:15:54 +0800

    Update p2p_config.h

--- p2p_config.h
+++ p2p_config.h
@@ -7,40 +7,40 @@
 #include <shellapi.h>
 #include <stdio.h>
 #include <ws2tcpip.h>
+#include <gdiplus.h> // 必须包含 GDI+
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "gdi32.lib")
 #pragma comment(lib, "shell32.lib")
 #pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "gdiplus.lib") // 引入 GDI+ 库
 
-// --- 全局配置 ---
 #define ENABLE_TRAY   1
-
-// --- 资源 ID (必须与 resource.rc 对应) ---
 #define IDD_LOGIN     101
 #define IDC_EDIT_ID   1001
 #define ID_TRAY_EXIT  2001
 #define WM_TRAY_MSG   (WM_USER + 2)
 
-// --- STUN 配置 (按 2026-01-15 要求) ---
 static const char* STUN_SERVERS[] = {
     "stun.qq.com:3478", "stun.aliyun.com:3478", "stun.huawei.com:3478",
     "stun.cloudflare.com:3478", "stun.l.google.com:19302", "stun1.l.google.com:19302",
     "stun2.l.google.com:19302", "stun3.l.google.com:19302", "stun4.l.google.com:19302"
 };
-#define STUN_COUNT 9  // 修复 server.c 报错的核心定义
+#define STUN_COUNT 9
 
 #define P2P_PORT      9000
 #define AUTH_MAGIC    0xABCDEF12
-#define CHUNK_SIZE    1024 
+#define CHUNK_SIZE    1200 // 略微调大包体积，提高传输效率
 
 #pragma pack(push, 1)
 typedef struct {
     unsigned int magic;
-    int type; // 1: CMD, 2: SCREEN
-    int x, y; 
-    int slice_size;
+    int type;       // 1: CMD, 2: SCREEN_DATA
+    int frame_id;   // 帧序号，防止乱序
+    int offset;     // 在整帧 JPEG 里的偏移
+    int total_size; // 这一帧 JPEG 的总大小
+    int slice_size; // 本包载荷大小
     unsigned char data[CHUNK_SIZE];
 } P2PPacket;
 #pragma pack(pop)
