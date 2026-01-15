4719c7917065784e7fed0a800bec1dd9343d7839
GoGoogle <wowjoint@gmail.com>
2026-01-15 15:56:39 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,84 +1,62 @@
 #include "p2p_config.h"
 
-typedef void GpBitmap;
-typedef void GpImage;
-typedef void GpGraphics;
-typedef struct { UINT32 v; PVOID p1; BOOL b1, b2; } Gsi;
-
-extern __declspec(dllimport) int WINAPI GdiplusStartup(ULONG_PTR*, Gsi*, void*);
-extern __declspec(dllimport) int WINAPI GdipCreateBitmapFromStream(IStream*, GpBitmap**);
-extern __declspec(dllimport) int WINAPI GdipCreateFromHDC(HDC, GpGraphics**);
-extern __declspec(dllimport) int WINAPI GdipDrawImageRectI(GpGraphics*, GpImage*, int, int, int, int);
-extern __declspec(dllimport) int WINAPI GdipDeleteGraphics(GpGraphics*);
-extern __declspec(dllimport) int WINAPI GdipDisposeImage(GpImage*);
-
 SOCKET g_sock;
 struct sockaddr_in g_srv;
 HWND g_mainWnd;
 NOTIFYICONDATAW g_nid = {0};
-unsigned char* g_recvBuf = NULL;
-int g_lastFid = 0;
-GpBitmap* g_dispBmp = NULL;
-ULONG_PTR g_gdiToken;
+unsigned char* g_screenBuf = NULL;
+int g_scrW = 1920, g_scrH = 1080; 
 
 DWORD WINAPI RecvThread(LPVOID lp) {
-    g_recvBuf = (unsigned char*)malloc(1024 * 1024);
-    P2PPacket p; struct sockaddr_in f; int fl = sizeof(f);
+    g_screenBuf = (unsigned char*)malloc(3840 * 2160 * 4);
+    P2PPacket pkt; struct sockaddr_in f; int fl = sizeof(f);
     while(1) {
-        int r = recvfrom(g_sock, (char*)&p, sizeof(p), 0, (struct sockaddr*)&f, &fl);
-        if(r > 0 && p.magic == AUTH_MAGIC && p.type == 2) {
-            if (p.frame_id >= g_lastFid) {
-                memcpy(g_recvBuf + p.offset, p.data, p.slice_size);
-                if (p.offset + p.slice_size >= p.total_size) {
-                    IStream* s = SHCreateMemStream(g_recvBuf, p.total_size);
-                    if (s) {
-                        GpBitmap* nb = NULL;
-                        if (GdipCreateBitmapFromStream(s, &nb) == 0) {
-                            if (g_dispBmp) GdipDisposeImage((GpImage*)g_dispBmp);
-                            g_dispBmp = nb; InvalidateRect(g_mainWnd, NULL, FALSE);
-                        }
-                        s->lpVtbl->Release(s);
-                    }
-                    g_lastFid = p.frame_id;
-                }
+        int r = recvfrom(g_sock, (char*)&pkt, sizeof(pkt), 0, (struct sockaddr*)&f, &fl);
+        if(r > 0 && pkt.magic == AUTH_MAGIC && pkt.type == 2) {
+            if(pkt.x + pkt.slice_size <= 3840 * 2160 * 4) {
+                memcpy(g_screenBuf + pkt.x, pkt.data, pkt.slice_size);
+                if (pkt.x + pkt.slice_size >= pkt.y - CHUNK_SIZE) InvalidateRect(g_mainWnd, NULL, FALSE);
             }
         }
     }
     return 0;
 }
 
-LRESULT CALLBACK MainProc(HWND h, UINT m, WPARAM w, LPARAM l) {
+LRESULT CALLBACK MainWndProc(HWND h, UINT m, WPARAM w, LPARAM l) {
     if (m == WM_PAINT) {
         PAINTSTRUCT ps; HDC hdc = BeginPaint(h, &ps);
-        if (g_dispBmp) {
-            GpGraphics* g = NULL; GdipCreateFromHDC(hdc, &g);
+        if(g_screenBuf) {
+            BITMAPINFO bmi = {0};
+            bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+            bmi.bmiHeader.biWidth = g_scrW; bmi.bmiHeader.biHeight = -g_scrH;
+            bmi.bmiHeader.biPlanes = 1; bmi.bmiHeader.biBitCount = 32;
             RECT rc; GetClientRect(h, &rc);
-            GdipDrawImageRectI(g, (GpImage*)g_dispBmp, 0, 0, rc.right, rc.bottom);
-            GdipDeleteGraphics(g);
+            StretchDIBits(hdc, 0, 0, rc.right, rc.bottom, 0, 0, g_scrW, g_scrH, g_screenBuf, &bmi, DIB_RGB_COLORS, SRCCOPY);
         }
-        HBRUSH br = CreateSolidBrush(RGB(0,255,0)); SelectObject(hdc, br);
-        Ellipse(hdc, 10, 10, 20, 20); DeleteObject(br);
+        HBRUSH br = CreateSolidBrush(CLR_ACTIVE); SelectObject(hdc, br);
+        Ellipse(hdc, 10, 10, 25, 25); DeleteObject(br); // 状态指示点
         EndPaint(h, &ps); return 0;
     }
     if (m == WM_LBUTTONDOWN || m == WM_RBUTTONDOWN || m == WM_LBUTTONDBLCLK) {
         RECT rc; GetClientRect(h, &rc);
-        int rx = (LOWORD(l) * GetSystemMetrics(SM_CXSCREEN)) / (rc.right ? rc.right : 1);
-        int ry = (HIWORD(l) * GetSystemMetrics(SM_CYSCREEN)) / (rc.bottom ? rc.bottom : 1);
+        int rx = (LOWORD(l) * g_scrW) / (rc.right ? rc.right : 1);
+        int ry = (HIWORD(l) * g_scrH) / (rc.bottom ? rc.bottom : 1);
         int type = (m == WM_LBUTTONDOWN) ? 1 : (m == WM_RBUTTONDOWN ? 3 : 5);
-        P2PPacket p = { AUTH_MAGIC, type, 0, rx, ry };
-        sendto(g_sock, (char*)&p, sizeof(p), 0, (struct sockaddr*)&g_srv, sizeof(g_srv));
+        P2PPacket pkt = { AUTH_MAGIC, type, rx, ry, 0 };
+        sendto(g_sock, (char*)&pkt, sizeof(pkt), 0, (struct sockaddr*)&g_srv, sizeof(g_srv));
     }
     if (m == WM_KEYDOWN || m == WM_KEYUP) {
-        P2PPacket p = { AUTH_MAGIC, 4, 0, (int)w, (m == WM_KEYUP ? KEYEVENTF_KEYUP : 0) };
-        sendto(g_sock, (char*)&p, sizeof(p), 0, (struct sockaddr*)&g_srv, sizeof(g_srv));
+        P2PPacket pkt = { AUTH_MAGIC, 4, (int)w, (m == WM_KEYUP ? KEYEVENTF_KEYUP : 0), 0 };
+        sendto(g_sock, (char*)&pkt, sizeof(pkt), 0, (struct sockaddr*)&g_srv, sizeof(g_srv));
     }
     if (m == WM_TRAY_MSG && l == WM_RBUTTONUP) {
-        POINT p; GetCursorPos(&p); HMENU hm = CreatePopupMenu();
-        AppendMenuW(hm, MF_STRING, ID_TRAY_EXIT, L"退出控制端");
-        SetForegroundWindow(h); TrackPopupMenu(hm, TPM_RIGHTBUTTON, p.x, p.y, 0, h, NULL);
-        DestroyMenu(hm);
+        POINT p; GetCursorPos(&p); HMENU hMenu = CreatePopupMenu();
+        AppendMenuW(hMenu, MF_STRING, ID_TRAY_EXIT, L"退出控制端");
+        SetForegroundWindow(h); TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, p.x, p.y, 0, h, NULL);
+        DestroyMenu(hMenu);
     }
     if (m == WM_COMMAND && LOWORD(w) == ID_TRAY_EXIT) { Shell_NotifyIconW(NIM_DELETE, &g_nid); ExitProcess(0); }
+    if (m == WM_DESTROY) { Shell_NotifyIconW(NIM_DELETE, &g_nid); PostQuitMessage(0); }
     return DefWindowProc(h, m, w, l);
 }
 
@@ -95,13 +73,12 @@ INT_PTR CALLBACK DlgProc(HWND h, UINT m, WPARAM w, LPARAM l) {
 
 int APIENTRY WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR lp, int nS) {
     WSADATA wsa; WSAStartup(0x0202, &wsa);
-    Gsi gsi = {1, NULL, FALSE, FALSE}; GdiplusStartup(&g_gdiToken, &gsi, NULL);
     if (DialogBoxParamA(hI, MAKEINTRESOURCEA(IDD_LOGIN), NULL, DlgProc, 0) != IDOK) return 0;
     g_sock = socket(AF_INET, SOCK_DGRAM, 0);
-    WNDCLASSW wc = {0}; wc.lpfnWndProc = MainProc; wc.hInstance = hI; wc.lpszClassName = L"View";
+    WNDCLASSW wc = {0}; wc.lpfnWndProc = MainWndProc; wc.hInstance = hI; wc.lpszClassName = L"P2P_View";
     wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.style = CS_DBLCLKS; RegisterClassW(&wc);
-    g_mainWnd = CreateWindowW(L"View", L"P2P Control", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 960, 540, 0, 0, hI, 0);
+    g_mainWnd = CreateWindowW(L"P2P_View", L"Remote Desktop Control", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 960, 540, 0, 0, hI, 0);
     g_nid.cbSize = sizeof(g_nid); g_nid.hWnd = g_mainWnd; g_nid.uID = 1; g_nid.uFlags = NIF_ICON|NIF_MESSAGE|NIF_TIP;
     g_nid.uCallbackMessage = WM_TRAY_MSG; g_nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wcscpy(g_nid.szTip, L"P2P Control"); Shell_NotifyIconW(NIM_ADD, &g_nid);
