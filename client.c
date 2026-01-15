2b9e2c3309392592ee66140c72df3543508c1d2c
GoGoogle <wowjoint@gmail.com>
2026-01-15 08:40:22 +0800

    Update client.c

--- client.c
+++ client.c
@@ -3,6 +3,7 @@
 COLORREF g_clr = CLR_TRY;
 struct sockaddr_in g_target;
 SOCKET g_sock;
+char g_input_target[64] = {0};
 
 void DrawDot() {
     HDC h = GetDC(NULL); POINT p; GetCursorPos(&p);
@@ -11,30 +12,33 @@ void DrawDot() {
     DeleteObject(b); ReleaseDC(NULL, h);
 }
 
-void Probe() {
-    int o = 1; setsockopt(g_sock, SOL_SOCKET, SO_BROADCAST, (char*)&o, sizeof(o));
-    struct sockaddr_in ba = { AF_INET, htons(LAN_PORT), INADDR_BROADCAST };
-    sendto(g_sock, "SCAN", 4, 0, (struct sockaddr*)&ba, sizeof(ba));
-    for(int i=0; i<STUN_COUNT; i++) {
-        char host[64]; int port; sscanf(STUN_SERVERS[i], "%[^:]:%d", host, &port);
-        struct hostent* he = gethostbyname(host);
-        if(he) {
-            struct sockaddr_in sa = { AF_INET, htons(port) };
-            memcpy(&sa.sin_addr, he->h_addr, he->h_length);
-            sendto(g_sock, "STUN", 4, 0, (struct sockaddr*)&sa, sizeof(sa));
-        }
-    }
-    struct timeval t = {0, 800000}; setsockopt(g_sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&t, sizeof(t));
-    struct sockaddr_in f; int fl = sizeof(f); char b[64];
-    if(recvfrom(g_sock, b, 64, 0, (struct sockaddr*)&f, &fl) > 0) {
-        g_target = f; g_target.sin_port = htons(P2P_PORT);
-        g_clr = (f.sin_addr.S_un.S_un_b.s_b1 == 192) ? CLR_LAN : CLR_WAN;
+void Probe(char* target) {
+    struct hostent* he = gethostbyname(target);
+    if(he) {
+        g_target.sin_family = AF_INET;
+        g_target.sin_port = htons(P2P_PORT);
+        memcpy(&g_target.sin_addr, he->h_addr, he->h_length);
+        g_clr = CLR_WAN;
     } else g_clr = CLR_ERR;
 }
 
-int main() {
+INT_PTR CALLBACK DlgProc(HWND h, UINT m, WPARAM w, LPARAM l) {
+    if(m == WM_COMMAND) {
+        if(LOWORD(w) == IDOK) {
+            GetDlgItemTextA(h, 101, g_input_target, 64);
+            EndDialog(h, IDOK);
+        } else if(LOWORD(w) == IDCANCEL) EndDialog(h, IDCANCEL);
+    }
+    return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR lp, int nS) {
     WSADATA w; WSAStartup(0x0202, &w);
-    g_sock = socket(AF_INET, SOCK_DGRAM, 0); Probe();
+    if(DialogBoxA(hI, (LPCSTR)MAKEINTRESOURCE(0), NULL, DlgProc) != IDOK) return 0;
+
+    g_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    Probe(g_input_target);
+
     while(1) {
         DrawDot();
         if(GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
@@ -42,7 +46,7 @@ int main() {
             P2PPacket pkt = { AUTH_MAGIC, 2, p.x, p.y };
             sendto(g_sock, (char*)&pkt, sizeof(pkt), 0, (struct sockaddr*)&g_target, sizeof(g_target));
         }
-        // 此处可添加接收服务端剪贴板逻辑 (参考 server.c 处理 type 3)
+        if(GetAsyncKeyState(VK_ESCAPE)) break;
         Sleep(20);
     }
     return 0;
