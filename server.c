e6ad12541a32bea13534cd24ff4958ff67613a33
GoGoogle <wowjoint@gmail.com>
2026-01-15 14:04:32 +0800

    Update server.c

--- server.c
+++ server.c
@@ -1,6 +1,6 @@
 #include "p2p_config.h"
 
-// GDI+ C 接口定义
+// --- GDI+ 纯 C 接口手动声明 (防止 cl.exe 找不到定义) ---
 typedef void GpBitmap;
 typedef void GpImage;
 typedef struct { UINT32 v; PVOID p1; BOOL b1, b2; } Gsi;
@@ -19,33 +19,33 @@ BOOL g_hasClient = FALSE;
 NOTIFYICONDATAW g_nid = { 0 };
 ULONG_PTR g_gdiToken;
 
-int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
+int GetEncoder(const WCHAR* format, CLSID* pClsid) {
     UINT num = 0, size = 0;
     GdipGetImageEncodersSize(&num, &size);
-    GpCodec* pIci = (GpCodec*)malloc(size);
-    GdipGetImageEncoders(num, size, pIci);
+    GpCodec* p = (GpCodec*)malloc(size);
+    GdipGetImageEncoders(num, size, p);
     for (UINT j = 0; j < num; ++j) {
-        if (wcscmp(pIci[j].MimeType, format) == 0) {
-            *pClsid = pIci[j].Clsid;
-            free(pIci); return j;
+        if (wcscmp(p[j].MimeType, format) == 0) {
+            *pClsid = p[j].Clsid;
+            free(p); return j;
         }
     }
-    free(pIci); return -1;
+    free(p); return -1;
 }
 
-void GetIPs(char* loc, char* pub) {
-    strcpy(pub, "探测中...");
-    char host[256]; gethostname(host, 256);
-    struct hostent* he = gethostbyname(host);
+void GetIPInfo(char* out) {
+    char loc[64] = "127.0.0.1", pub[64] = "探测失败";
+    char name[256]; gethostname(name, 256);
+    struct hostent* he = gethostbyname(name);
     if (he) strcpy(loc, inet_ntoa(*(struct in_addr*)he->h_addr));
-
+    
     SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
     int to = 800; setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (char*)&to, sizeof(to));
     for (int i = 0; i < STUN_COUNT; i++) {
-        char sh[64]; int sp; sscanf(STUN_SERVERS[i], "%[^:]:%d", sh, &sp);
-        struct hostent* she = gethostbyname(sh);
+        char h[64]; int p; sscanf(STUN_SERVERS[i], "%[^:]:%d", h, &p);
+        struct hostent* she = gethostbyname(h);
         if (!she) continue;
-        struct sockaddr_in sa = { AF_INET, htons(sp), *((struct in_addr*)she->h_addr) };
+        struct sockaddr_in sa = { AF_INET, htons(p), *((struct in_addr*)she->h_addr) };
         unsigned char req[20] = {0}; *(unsigned short*)req = htons(0x0001); *(unsigned int*)(req+4) = htonl(0x2112A442);
         sendto(s, (char*)req, 20, 0, (struct sockaddr*)&sa, sizeof(sa));
         unsigned char buf[512]; struct sockaddr_in f; int fl = sizeof(f);
@@ -53,22 +53,23 @@ void GetIPs(char* loc, char* pub) {
             for (int j = 20; j < 500; j++) {
                 if (buf[j] == 0x00 && buf[j+1] == 0x01) {
                     struct in_addr a; a.S_un.S_addr = *(unsigned int*)(buf + j + 8);
-                    strcpy(pub, inet_ntoa(a)); closesocket(s); return;
+                    strcpy(pub, inet_ntoa(a)); break;
                 }
             }
+            if (strcmp(pub, "探测失败") != 0) break;
         }
     }
     closesocket(s);
+    sprintf(out, "内网: %s\n公网: %s", loc, pub);
 }
 
 DWORD WINAPI ScreenThread(LPVOID lp) {
     int w = GetSystemMetrics(SM_CXSCREEN), h = GetSystemMetrics(SM_CYSCREEN);
     HDC hdcScr = GetDC(NULL);
-    CLSID jpgClsid; GetEncoderClsid(L"image/jpeg", &jpgClsid);
-    int fid = 0;
+    CLSID jpgClsid; GetEncoder(L"image/jpeg", &jpgClsid);
+    int frame_id = 0;
     while (1) {
         if (!g_hasClient) { Sleep(200); continue; }
-        fid++;
         HDC hdcMem = CreateCompatibleDC(hdcScr);
         HBITMAP hBmp = CreateCompatibleBitmap(hdcScr, w, h);
         SelectObject(hdcMem, hBmp);
@@ -76,18 +77,20 @@ DWORD WINAPI ScreenThread(LPVOID lp) {
         GpBitmap* bmp = NULL; GdipCreateBitmapFromHBITMAP(hBmp, NULL, &bmp);
         IStream* stream = NULL; CreateStreamOnHGlobal(NULL, TRUE, &stream);
         GdipSaveImageToStream((GpImage*)bmp, stream, &jpgClsid, NULL);
+        
         HGLOBAL hg = NULL; GetHGlobalFromStream(stream, &hg);
-        unsigned char* d = (unsigned char*)GlobalLock(hg);
-        int sz = (int)GlobalSize(hg);
-        for (int i = 0; i < sz; i += CHUNK_SIZE) {
-            P2PPacket p = { AUTH_MAGIC, 2, fid, i, sz };
-            p.slice_size = (sz - i < CHUNK_SIZE) ? (sz - i) : CHUNK_SIZE;
-            memcpy(p.data, d + i, p.slice_size);
+        unsigned char* data = (unsigned char*)GlobalLock(hg);
+        int total = (int)GlobalSize(hg);
+        frame_id++;
+        for (int i = 0; i < total; i += CHUNK_SIZE) {
+            P2PPacket p = { AUTH_MAGIC, 2, frame_id, i, total };
+            p.slice_size = (total - i < CHUNK_SIZE) ? (total - i) : CHUNK_SIZE;
+            memcpy(p.data, data + i, p.slice_size);
             sendto(g_srvSock, (char*)&p, sizeof(p), 0, (struct sockaddr*)&g_clientAddr, sizeof(g_clientAddr));
         }
         GlobalUnlock(hg); stream->lpVtbl->Release(stream);
         GdipDisposeImage((GpImage*)bmp); DeleteObject(hBmp); DeleteDC(hdcMem);
-        Sleep(50); 
+        Sleep(40); 
     }
     return 0;
 }
@@ -107,9 +110,7 @@ LRESULT CALLBACK SrvProc(HWND hw, UINT m, WPARAM w, LPARAM l) {
 int APIENTRY WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR lp, int nS) {
     WSADATA wsa; WSAStartup(0x0202, &wsa);
     Gsi gsi = {1, NULL, FALSE, FALSE}; GdiplusStartup(&g_gdiToken, &gsi, NULL);
-    char lip[64], pip[64]; GetIPs(lip, pip);
-    wchar_t info[256]; swprintf(info, 256, L"服务端启动\n内网:%S\n公网:%S", lip, pip);
-    MessageBoxW(NULL, info, L"Service", MB_OK);
+    char info[256]; GetIPInfo(info); MessageBoxA(NULL, info, "Service", MB_OK);
 
     WNDCLASSW wc = {0}; wc.lpfnWndProc = SrvProc; wc.hInstance = hI; wc.lpszClassName = L"Srv";
     RegisterClassW(&wc); HWND hw = CreateWindowW(L"Srv", NULL, 0, 0, 0, 0, 0, 0, 0, hI, 0);
@@ -124,19 +125,18 @@ int APIENTRY WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR lp, int nS) {
 
     P2PPacket p; struct sockaddr_in f; int fl = sizeof(f);
     while (1) {
-        MSG msg; while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) { TranslateMessage(&msg); DispatchMessage(&msg); }
-        fd_set rs; FD_ZERO(&rs); FD_SET(g_srvSock, &rs); struct timeval tv = {0, 10000};
-        if (select(0, &rs, 0, 0, &tv) > 0) {
-            if (recvfrom(g_srvSock, (char*)&p, sizeof(p), 0, (struct sockaddr*)&f, &fl) > 0) {
-                if (p.magic == AUTH_MAGIC) {
-                    g_clientAddr = f; g_hasClient = TRUE;
-                    if (p.type == 1) { SetCursorPos(p.v1, p.v2); mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0); }
-                    if (p.type == 3) { SetCursorPos(p.v1, p.v2); mouse_event(MOUSEEVENTF_RIGHTDOWN|MOUSEEVENTF_RIGHTUP,0,0,0,0); }
-                    if (p.type == 5) { SetCursorPos(p.v1, p.v2); mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0); mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0); }
-                    if (p.type == 4) { keybd_event((BYTE)p.v1, 0, p.v2, 0); }
-                }
+        MSG msg; while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) DispatchMessage(&msg);
+        unsigned long mode = 1; ioctlsocket(g_srvSock, FIONBIO, &mode);
+        if (recvfrom(g_srvSock, (char*)&p, sizeof(p), 0, (struct sockaddr*)&f, &fl) > 0) {
+            if (p.magic == AUTH_MAGIC) {
+                g_clientAddr = f; g_hasClient = TRUE;
+                if (p.type == 1) { SetCursorPos(p.offset, p.total_size); mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0); }
+                if (p.type == 3) { SetCursorPos(p.offset, p.total_size); mouse_event(MOUSEEVENTF_RIGHTDOWN|MOUSEEVENTF_RIGHTUP,0,0,0,0); }
+                if (p.type == 4) { keybd_event((BYTE)p.offset, 0, p.total_size, 0); }
+                if (p.type == 5) { SetCursorPos(p.offset, p.total_size); mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0); mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0); }
             }
         }
+        Sleep(5);
     }
     return 0;
 }
